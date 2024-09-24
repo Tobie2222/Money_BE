8527b1bdc9cab469f7ff518beacf1389a22de997
Tobie2222 <kienvanvo7777@gmail.com>
2024-09-24 23:40:00 +0700

    update register

--- controller/authController.js
+++ controller/authController.js
@@ -26,28 +26,31 @@ class AuthController {
                 name: Joi.string().required(),
                 email: Joi.string().email().required(),
                 password: Joi.string().min(6).required(),
+                confirmPassword: Joi.string().valid(Joi.ref('password')).required().messages({
+                    'any.only': 'Mật khẩu xác nhận không khớp',
+                }),
                 gender: Joi.string().valid('male', 'female', 'other').required(),
             });
-            const { name, email, password, gender } = req.body;
+    
+            const { name, email, password, confirmPassword, gender } = req.body;
             const { error } = schema.validate(req.body);
             if (error) {
                 return res.status(400).json({ message: error.details[0].message });
             }
-        
-
+    
             // Check if email already exists
             const existingUser = await User.findOne({ where: { email } });
             if (existingUser) {
                 return res.status(400).json({ message: 'Email đã tồn tại' });
             }
-
+    
             // Hash password before saving
             const salt = await bcrypt.genSalt(10);
             const hashedPassword = await bcrypt.hash(password, salt);
-
+    
             // Create user slug
             const userSlug = slug(name, { lower: true });
-
+    
             // Create new user
             const newUser = await User.create({
                 name,
@@ -56,16 +59,17 @@ class AuthController {
                 gender,
                 slug_user: userSlug
             });
-
+    
             return res.status(201).json({
                 message: 'Tạo người dùng thành công',
                 userId: newUser.id
             });
-
+    
         } catch (err) {
             return res.status(500).json({ message: `Lỗi: ${err.message || err}` });
         }
     }
+    
 
     // Login user
     async login(req, res) {
